8a51182dfd60196e1911f0d7925bd7f19a2ec2ad
gouravKJ <gouravjaiswal2006@gmail.com>
2025-06-03 11:28:22 +0530

    updated

---
+++ images/submit.js
@@ -0,0 +1,7 @@
+let popup=document.getElementById("popup");
+function openpup(){
+    popup.classList.add("open-pop");//css link
+}
+function closepup(){
+    popup.classList.add("open-pop");//css link
+}
